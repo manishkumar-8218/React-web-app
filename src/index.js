a118c6ec909074ed7b418f1fee50a8cc07e22ebd
[manish kumar] <[mrharpalsingh1980@gmail.com]>
2022-02-16 13:15:15 +0530

    adding react error overlay

--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { stopReportingRuntimeErrors } from "react-error-overlay";
 
+if (process.env.NODE_ENV === "development") {
+  stopReportingRuntimeErrors(); // disables error overlays
+}
 ReactDOM.render(
   <React.StrictMode>
     <App />
